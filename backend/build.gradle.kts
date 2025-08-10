d39e39de31168c4faf3469b89ef34dad724ac5ad
Matheus Rossetti <13912403+MRossettiPQ@users.noreply.github.com>
2025-08-10 14:31:13 -0300

    chore: update quarkus

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,7 +20,7 @@ apply(from = "pem.gradle.kts")
 //apply(from = "vue.gradle.kts")
 
 plugins {
-    val kotlinVersion = "2.0.21"
+    val kotlinVersion = "2.2.0"
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
     kotlin("plugin.allopen") version kotlinVersion
